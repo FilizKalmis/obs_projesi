d8f33d089b5aafd477b08cfb711f4fe2ccbe3277
Filiz Kalmis <filizkalmis45@gmail.com>
2026-04-21 01:22:18 +0300

    İlk commit: ASP.NET Core Projesi

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace OBS_Projesi.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
